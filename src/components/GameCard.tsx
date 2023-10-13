e9c3478f0cb1b64c45771113c209e20110239e1f
Lucas Jaensisch <LucasJaenisch@users.noreply.github.com>
2023-10-13 10:44:08 -0300

    Get optimized images with the API support

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,8 +1,9 @@
-import { Text, Image, Badge, HStack } from "@chakra-ui/react";
+import { Image, HStack } from "@chakra-ui/react";
 import { Card, Stack, Heading, CardBody } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -11,17 +12,15 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Stack mt="6" spacing="3">
-          <HStack justify="space-between">
-            <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            />
-            <CriticScore score={game.metacritic} />
-          </HStack>
-          <Heading size="md">{game.name}</Heading>
-        </Stack>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        <Heading size="md">{game.name}</Heading>
       </CardBody>
     </Card>
   );
